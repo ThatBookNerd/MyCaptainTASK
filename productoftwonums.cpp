f492557e0337f3e52b508360ba197c637ed76e42
ThatBookNerd <87295837+ThatBookNerd@users.noreply.github.com>
2021-07-12 11:23:11 +0530

    Created productoftwonums.cpp
    The program goes ahead and prints product of two numbers in a C++ window by taking inputs from the user.

---
+++ productoftwonums.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float x,y,product;
+    cout<<"Enter your first number: ";
+    cin>>x;
+    cout<<endl<<"Enter your second number: ";
+    cin>>y;
+    product= x*y;
+    cout<<endl<<"The product of the two numbers given: "<<product<<endl;
+
+}
