11e09b001b1f47847e1f989256cc48c572b69b2a
ThatBookNerd <87295837+ThatBookNerd@users.noreply.github.com>
2021-07-12 11:21:36 +0530

    Created sizeofdatatypes.cpp
    This file prints the size of the different types of data types, mostly basic, like the int, char, float, and double in a c++ window.

---
+++ sizeofdatatypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int Typeofinteger;
+    char Typeofcharacter;
+    float Typeoffloat;
+    double Typeofdouble
+    cout<<"Size of character data type is: "<<sizeof(Typeofinteger)<<" byte."<<endl;
+    cout<<"Size of integer data type is: "<<sizeof(Typeofcharacter)<<" bytes."<<endl;
+    cout<<"Size of float data type is: "<<sizeof(Typeoffloat)<<" bytes."<<endl;
+    cout<<"Size of double data type is: "<<sizeof(Typeofdouble)<<" bytes."<<endl;
+}
